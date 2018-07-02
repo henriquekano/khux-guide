2522ee58356d59f26f2433f13aa891d3ffea4d2c
Henrique Kano <henriquekano@hotmail.com>
2018-07-01 21:04:11 -0300

    filter: add sample modal for filters

--- khux-guide/App.js
+++ khux-guide/App.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import { createStackNavigator } from 'react-navigation'
 import MedalFilter from './src/screens/MedalFilter'
+import FilterModal from './src/components/FilterModal'
 
-export default createStackNavigator({
+const MainStack = createStackNavigator({
   Home: {
-    screen: MedalFilter
-  }
+    screen: MedalFilter,
+    navigationOptions: {
+      header: null
+    }
+  },
 })
+
+const RootStack = createStackNavigator(
+  {
+    Main: {
+      screen: MainStack
+    },
+    MyModal: {
+      screen: FilterModal,
+    },  
+  },
+  {
+    mode: 'modal',
+    headerMode: 'none',
+  }
+);
+
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
--- khux-guide/src/screens/MedalFilter.js
+++ khux-guide/src/screens/MedalFilter.js
@@ -1,53 +1,71 @@
 import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native'
+import {
+    StyleSheet, View, TouchableOpacity, Text, Button
+} from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
 import { filterItens } from '../khuxbotApiCaller'
 import Medal from '../components/Medal'
 
 export default class MedalFilter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      itens: []
-    }
-  }
-
-  componentDidMount() {
-    this.loadMedals()
-  }
-
-  loadMedals = () => {
-    return filterItens({
-      "filter": {
-        "tier": {
-          "min": 4,
-          "max": 6
+    constructor(props) {
+        super(props)
+        this.state = {
+            itens: []
         }
-      }
-    })
-    .then(itens => this.setState({ itens }))
-    .catch(console.log)
-}
+    }
 
-  render() {
-    const { itens } = this.state
-    return (
-      <View style={ styles.container }>
-        <FlatList
-          data={ itens }
-          keyExtractor={ (item) => item.id.toString() }
-          renderItem={ ({item}) => 
-            <Medal
-              medal={ item }
-            />
-          }
-        />
-      </View>
-    );
-  }
+    componentDidMount() {
+        this.loadMedals()
+    }
+
+    loadMedals = () => {
+        return filterItens({
+            "filter": {
+                "tier": {
+                    "min": 4,
+                    "max": 6
+                }
+            }
+        })
+            .then(itens => this.setState({ itens }))
+            .catch(console.log)
+    }
+
+    render() {
+        const { itens } = this.state
+        const { navigation } = this.props
+        return (
+            <View style={styles.container}>
+                <TouchableOpacity
+                    onPress={ () => navigation.navigate('MyModal') }
+                >
+                    <Text style={styles.filterInput}>
+                        Filters
+                    </Text>
+                </TouchableOpacity>
+
+                <FlatList
+                    data={itens}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) =>
+                        <Medal
+                            medal={item}
+                        />
+                    }
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-  },
+    container: {
+        marginTop: 23
+    },
+    filterInput: {
+        borderRadius: 10,
+        margin: 10,
+        padding: 10,
+        elevation: 3,
+    }
 });
